5784b09d5ecfff0d342d5236d9351d0aea04ca90
cobaltinferno <nil>
2023-01-04 17:54:04 -0500

    Update alacritty

--- src/Berry/Themes/Alacritty.hs
+++ src/Berry/Themes/Alacritty.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Berry.Themes.Alacritty
   ( AlacrittyThemes(..)
+  , xInfix
   ) where
 
 import Berry.Database.Themes (getAlacritty)
-import System.Directory (copyFile)
+import Data.List (isInfixOf)
+import System.Directory (copyFile, doesFileExist)
 import System.Environment (getEnv)
 
 data AlacrittyThemes
@@ -11,9 +15,15 @@ data AlacrittyThemes
   | Nord
   deriving (Show, Eq)
 
+getConfig' :: IO String
+getConfig' =
+  getEnv "HOME" >>= \v ->
+    return $ v <> "/.config/alacritty/alacritty.yml"
+
 getConfig :: IO String
 getConfig =
-  getEnv $ "HOME" ++ "/.config/alacritty/berryfile.yml"
+  getEnv "HOME" >>= \v ->
+    return $ v <> "/.config/alacritty/berryfile.yml"
 
 setThemeFile :: AlacrittyThemes -> IO ()
 setThemeFile theme =
@@ -26,3 +36,19 @@ setThemeFile theme =
       getConfig >>= \conf ->
         getAlacritty >>= \alacritty ->
           copyFile (alacritty ++ "nord.yml") conf
+
+xInfix :: IO Bool
+xInfix =
+  getConfig >>= \conf ->
+    doesFileExist conf >>= \case
+      True ->
+        getConfig >>= readFile >>= \x ->
+          return $ isInfixOf "berryfile" x
+      False -> return False
+
+text :: IO ()
+text =
+  xInfix >>= \case
+    False ->
+      getConfig >>= \x ->
+        getConfig' >>= \v -> appendFile v $ "import: " ++ x
