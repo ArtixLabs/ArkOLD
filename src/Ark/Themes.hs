b7a88ba346167b53e2aabcabafcbac6bffe864de
cobaltinferno <nil>
2023-01-05 01:21:02 -0500

    Add proper fzf

--- src/Ark/Themes.hs
+++ src/Ark/Themes.hs
@@ -6,5 +6,6 @@ module Ark.Themes
   ) where
 
 import Ark.Themes.Alacritty (alacrittyThemeFile)
+import Ark.Themes.Fzf (fzfSetTheme)
 import Ark.Themes.Kitty (kittyThemeFile)
 import Ark.Themes.Options (Themes(..))
