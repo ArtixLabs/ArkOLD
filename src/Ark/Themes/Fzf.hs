5746dc0f271841487cb998fe5ac9bd65a9cf0757
cobaltinferno <nil>
2023-01-05 01:24:56 -0500

    Add newline to end of fzf

--- src/Ark/Themes/Fzf.hs
+++ src/Ark/Themes/Fzf.hs
@@ -12,11 +12,11 @@ fzfSetTheme :: Themes -> IO ()
 fzfSetTheme Onedark =
   detectShell >>= \shell ->
     appendToShellConfig
-      "export FZF_DEFAULT_OPTS='--color=fg:#abb2bf,bg:#21252b,hl:#61afef --color=fg+:#abb2bf,bg+:#21252b,hl+:#61afef --color=info:#e5c07b,prompt:#e06c75,pointer:#e06c75 --color=marker:#98c379,spinner:#c678dd,header:#56b6c2'"
+      "export FZF_DEFAULT_OPTS='--color=fg:#abb2bf,bg:#21252b,hl:#61afef --color=fg+:#abb2bf,bg+:#21252b,hl+:#61afef --color=info:#e5c07b,prompt:#e06c75,pointer:#e06c75 --color=marker:#98c379,spinner:#c678dd,header:#56b6c2'\n"
       shell
 fzfSetTheme Nord =
   detectShell >>= \shell ->
     appendToShellConfig
-      "export FZF_DEFAULT_OPTS='--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'"
+      "export FZF_DEFAULT_OPTS='--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'\n"
       shell
 fzfSetTheme Undefined = return ()
