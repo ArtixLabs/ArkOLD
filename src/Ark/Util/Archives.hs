ab50510c80f9fb04037eca88cd68bbb5954bb864
cobaltinferno <nil>
2023-01-05 11:34:51 -0500

    Add beginning of tar

--- src/Ark/Util/Archives.hs
+++ src/Ark/Util/Archives.hs
@@ -6,6 +6,7 @@ module Ark.Util.Archives
 
 import Ark.Database.Network (curl, mkdir)
 import System.Directory (doesFileExist)
+import System.FilePath.Posix (takeExtensions)
 import System.Process (callCommand)
 
 verifyFiles :: String -> IO Bool
@@ -19,6 +20,25 @@ unzip' zipfile npath =
              True ->
                callCommand $
                "unzip " ++ zipfile ++ " -d " ++ npath
+             False ->
+               putStrLn $
+               "File: '" ++ zipfile ++ "' does not exist."
+      else putStrLn
+             "You need to download the 'unzip' program."
+
+tar' :: FilePath -> FilePath -> IO ()
+tar' zipfile npath =
+  mapM verifyFiles ["/usr/bin/tar", "/bin/tar"] >>= \x ->
+    if or x
+      then doesFileExist zipfile >>= \case
+             True ->
+               case (takeExtensions zipfile) of
+                 ".tar.gz" ->
+                   callCommand $
+                   "tar xvzf " ++ zipfile ++ " -C " ++ npath
+                 ".tar" ->
+                   callCommand $
+                   "tar xvf " ++ zipfile ++ " -C " ++ npath
              False ->
                putStrLn $
                "File: '" ++ zipfile ++ "' does not exist."
