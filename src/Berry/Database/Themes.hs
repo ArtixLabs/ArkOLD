e83dbbc621687691477feb35650b58f8eac250d3
cobaltinferno <nil>
2023-01-04 19:11:46 -0500

    Add framework for downloading files via --update

--- src/Berry/Database/Themes.hs
+++ src/Berry/Database/Themes.hs
@@ -2,9 +2,14 @@ module Berry.Database.Themes
   ( getAlacritty
   ) where
 
+import System.Directory
 import System.Environment (getEnv)
 
 getAlacritty :: IO String
 getAlacritty =
   getEnv "HOME" >>= \home ->
     return $ home ++ "/.berry/alacritty/"
+
+downloadFiles :: IO ()
+downloadFiles = do
+
